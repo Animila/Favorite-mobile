4a73788a6d8cb50636f9e7c6eeabe54a4c5172ec
Animila <hristoforovila5@gmail.com>
2022-04-22 23:07:21 +0900

    Add next page of StartPage

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+//подключаем концепцию/подход UI
+import 'package:favorite/screens/startupScreen.dart';
+import 'package:flutter/material.dart';
+
+//точка входа в приложение (базовый скелет)
+void main() => runApp(FavoruteApp());
+
+class FavoruteApp extends StatelessWidget {
+  //context - параметры компонетов
+  @override
+  Widget build(BuildContext context) {
+    // основа приложения
+    return MaterialApp(
+        title: 'Фаворит Мобайл',
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(225, 176, 48, 1),
+          textTheme: TextTheme(titleMedium: TextStyle(color: Colors.white)),
+        ),
+        home: StartPage());
+  }
+}
